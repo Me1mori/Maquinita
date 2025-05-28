be4a359d7c5f4c3be9f35f30ef7f036b32eb14f4
Me1mori <ssalvarezleonardoaa@gmail.com>
2025-05-27 22:31:57 -0700

    agregando los temas claro y oscuro para teléfono

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 document.getElementById("maquinita-pxf").onclick = function() {
     window.location.href = "PXF-Gashapon.html";
 }
+
+const body = document.body;
+const lightBtn = document.getElementById("lightMode");
+const darkBtn = document.getElementById("darkMode");
+
+lightBtn.addEventListener("click", () => {
+    body.classList.remove("dark-theme");
+    body.classList.add("light-theme");
+});
+
+darkBtn.addEventListener("click", () => {
+    body.classList.remove("light-theme");
+    body.classList.add("dark-theme");
+});
